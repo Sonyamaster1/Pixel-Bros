9ab7127a9b197016001959498d800aa6c316ef22
Alexander Shipulin <sania8600@gmail.com>
2023-10-17 20:40:31 +0700

    feat: создан компонент шапки
    Задача: PIX-12

---
+++ packages/client/src/components/header/index.tsx
@@ -0,0 +1,27 @@
+import { FC } from 'react'
+import { Link } from 'react-router-dom'
+
+import IconLogo from '../../assets/logo.png'
+import { MenuItem } from './menuItem'
+
+import styles from './index.module.scss'
+
+export const Header: FC = () => {
+  return (
+    <header className={styles.header}>
+      <Link to="/" className={styles.logoLink}>
+        <img src={IconLogo} alt="Логотип" />
+      </Link>
+      <nav>
+        <ul className={styles.list}>
+          <MenuItem label="forum" href="/forum" />
+          <MenuItem label="leaderboard" href="/leaderboard" />
+          <MenuItem label="play" href="/game" />
+        </ul>
+      </nav>
+      <Link to="/login" className={styles.signinLink}>
+        signin
+      </Link>
+    </header>
+  )
+}
