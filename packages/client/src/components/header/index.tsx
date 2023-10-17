623a6bd4bf2410ea35d360c71f62b06b1e87455f
Alexander Shipulin <sania8600@gmail.com>
2023-10-17 21:13:50 +0700

    fix(Header): заменена растровая иконка на векторную logo.png -> logo.svg
    Задача: PIX-12

--- packages/client/src/components/header/index.tsx
+++ packages/client/src/components/header/index.tsx
@@ -1,7 +1,7 @@
 import { FC } from 'react'
 import { Link } from 'react-router-dom'
 
-import IconLogo from '../../assets/images/logo.png'
+import IconLogo from '../../assets/images/logo.svg'
 
 import { MenuItem } from './menuItem'
 import { AuthenticationButton } from './authenticationButton'
@@ -11,16 +11,18 @@ import styles from './index.module.scss'
 export const Header: FC = () => {
   return (
     <header className={styles.header}>
-      <Link to="/" className={styles.logoLink}>
-        <img src={IconLogo} alt="Логотип" />
-      </Link>
-      <nav>
-        <ul className={styles.list}>
-          <MenuItem label="forum" href="/forum" />
-          <MenuItem label="leaderboard" href="/leaderboard" />
-          <MenuItem label="play" href="/game" />
-        </ul>
-      </nav>
+      <div className={styles.leftContent}>
+        <Link to="/" className={styles.logoLink}>
+          <img src={IconLogo} alt="Логотип" />
+        </Link>
+        <nav>
+          <ul className={styles.list}>
+            <MenuItem label="forum" href="/forum" />
+            <MenuItem label="leaderboard" href="/leaderboard" />
+            <MenuItem label="play" href="/game" />
+          </ul>
+        </nav>
+      </div>
       <AuthenticationButton />
     </header>
   )
