eb4fbe932ddaeec7815c784c5e8ad3a1d4a6c742
Alexander Shipulin <sania8600@gmail.com>
2023-10-18 21:10:22 +0700

    feat: добавлен базовый layout приложения
    Задача: PIX-12

--- packages/client/src/layouts/index.tsx
+++ packages/client/src/layouts/index.tsx
@@ -1,3 +1,12 @@
-export const layout = () => {
-  return <a></a>
+import { FC } from 'react'
+import { Outlet } from 'react-router-dom'
+import { Header } from '../components/header'
+
+export const RootLayout: FC = () => {
+  return (
+    <>
+      <Header />
+      <Outlet />
+    </>
+  )
 }
