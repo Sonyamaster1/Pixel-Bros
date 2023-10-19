3190559811a8e59b767e0f2937fd88c4677274d9
Alexander Shipulin <sania8600@gmail.com>
2023-10-19 21:09:04 +0700

    feat(PlayPage): создан файл стилей
    Задача: PIX-21

--- packages/client/src/pages/play/index.tsx
+++ packages/client/src/pages/play/index.tsx
@@ -1,5 +1,7 @@
 import { FC } from 'react'
 
+import styles from './index.module.scss'
+
 export const PlayPage: FC = () => {
-  return <main>Страница начала игры</main>
+  return <main className={styles.playPage}>Страница начала игры</main>
 }
