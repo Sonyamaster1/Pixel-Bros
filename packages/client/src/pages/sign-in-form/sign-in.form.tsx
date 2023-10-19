63f8f38975c35f2ccad946c00a8197c6a2e68e9e
suhanov <suhanov@tochka.com>
2023-10-19 17:11:04 +0600

    PIX-16: Изменено поведение поля для сбора value всей формы

--- packages/client/src/pages/sign-in-form/sign-in.form.tsx
+++ packages/client/src/pages/sign-in-form/sign-in.form.tsx
@@ -5,18 +5,54 @@ import {
   FooterButton,
 } from '../../components/button/button.component'
 import { Form } from '../../components/form/form.component'
+import { ChangeEvent, useCallback, useState } from 'react'
 
-const handleClick = () => console.log('Жмякнули кнопочку')
 const handleSubmit = () => console.log('handleSubmit')
 
+type TFormValueType = {
+  login: string
+  password: string
+}
+
+const defaultFormValue: TFormValueType = {
+  login: '',
+  password: '',
+}
+
 export function SignInForm() {
+  const [formValue, setFormValue] = useState<TFormValueType>(defaultFormValue)
+
+  const handleClick = useCallback(() => console.log(formValue), [formValue])
+  const handleOnChangeField = useCallback(
+    (event: ChangeEvent<HTMLInputElement>) => {
+      setFormValue(prevFormValue => ({
+        ...prevFormValue,
+        [event.target.name]: event.target.value,
+      }))
+      console.log(formValue, 'formValue')
+    },
+    [formValue]
+  )
+
   return (
     <Form onSubmit={handleSubmit}>
       <EntityHeader title="Authorization" />
-      <Field placeholder="Login" inputType="text" />
-      <Field placeholder="Password" inputType="password" />
+      <Field
+        value={formValue['login']}
+        onChange={handleOnChangeField}
+        inputName="login"
+        placeholder="Login"
+        inputType="text"
+      />
+      <Field
+        value={formValue['password']}
+        onChange={handleOnChangeField}
+        inputName="password"
+        placeholder="Password"
+        inputType="password"
+      />
       <FooterButton
-        buttonType="submit"
+        buttonType="button"
         onClick={handleClick}
         title="Sign In"
         color={ButtonColors.BLUE}
