65788cfb40a34c99a1b1aa9a6416f9a378e68a4c
Alexander Shipulin <sania8600@gmail.com>
2023-10-19 22:37:51 +0700

    feat(PlayPage): добавлена верстка страницы начала игры
    Задача: PIX-21

--- packages/client/src/pages/play/index.tsx
+++ packages/client/src/pages/play/index.tsx
@@ -1,7 +1,15 @@
 import { FC } from 'react'
 
+import IconBird from '../../assets/images/yellowbird.svg'
+
 import styles from './index.module.scss'
 
 export const PlayPage: FC = () => {
-  return <main className={styles.playPage}>Страница начала игры</main>
+  return (
+    <main className={styles.playPage}>
+      <h2 className={styles.title}>Get Ready!</h2>
+      <img src={IconBird} className={styles.icon} alt="Иконка птицы" />
+      <p className={styles.description}>Press “space” for start</p>
+    </main>
+  )
 }
