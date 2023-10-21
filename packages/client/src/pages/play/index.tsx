6785720fc6be18756e82777e2ea2eea8d76db92c
Alexander Shipulin <sania8600@gmail.com>
2023-10-21 15:14:50 +0700

    feat(PlayPage): добавлена логика обработки нажатия на пробел
    Задача: PIX-24

--- packages/client/src/pages/play/index.tsx
+++ packages/client/src/pages/play/index.tsx
@@ -1,12 +1,34 @@
-import { FC, useState } from 'react'
+import { FC, useEffect, useState } from 'react'
 
 import { StartGame } from './startGame'
 import { EndGame } from './endGame'
 
 import styles from './index.module.scss'
 
+enum KeyCodes {
+  Space = 'Space',
+}
+
 export const PlayPage: FC = () => {
   const [isBeginGame, setIsBeginGame] = useState(true)
+
+  function handleStartGame(event: KeyboardEvent) {
+    if (isBeginGame && event.code === KeyCodes.Space) {
+      setIsBeginGame(false)
+    }
+  }
+
+  function handleRepeatGame() {
+    setIsBeginGame(true)
+  }
+
+  useEffect(() => {
+    document.addEventListener('keyup', handleStartGame)
+
+    return () => {
+      document.removeEventListener('keyup', handleStartGame)
+    }
+  }, [])
 
   return (
     <main className={styles.playPage}>
