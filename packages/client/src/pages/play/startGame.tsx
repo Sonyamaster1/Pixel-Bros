3e81d18fbdfbde39dd3f4adbe6908bf363463a06
Alexander Shipulin <sania8600@gmail.com>
2023-10-21 14:55:17 +0700

    feat: создан отдельный компонент начала игры
    Задача: PIX-21

---
+++ packages/client/src/pages/play/startGame.tsx
@@ -0,0 +1,15 @@
+import { FC } from 'react'
+
+import IconBird from '../../assets/images/yellowbird.svg'
+
+import styles from './startGame.module.scss'
+
+export const StartGame: FC = () => {
+  return (
+    <section className={styles.startGame}>
+      <h2 className={styles.title}>Get Ready!</h2>
+      <img src={IconBird} className={styles.icon} alt="Иконка птицы" />
+      <p className={styles.description}>Press “space” for start</p>
+    </section>
+  )
+}
