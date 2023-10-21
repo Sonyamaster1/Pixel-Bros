9dc254366c17334696473090b3d56074855ecec9
Alexander Shipulin <sania8600@gmail.com>
2023-10-21 14:56:04 +0700

    fix(PlayPage): заменена верстка начала игры на компонент
    Задача: PIX-24

--- packages/client/src/pages/play/index.tsx
+++ packages/client/src/pages/play/index.tsx
@@ -1,15 +1,15 @@
-import { FC } from 'react'
+import { FC, useState } from 'react'
 
-import IconBird from '../../assets/images/yellowbird.svg'
+import { StartGame } from './startGame'
 
 import styles from './index.module.scss'
 
 export const PlayPage: FC = () => {
+  const [isBeginGame, setIsBeginGame] = useState(true)
+
   return (
     <main className={styles.playPage}>
-      <h2 className={styles.title}>Get Ready!</h2>
-      <img src={IconBird} className={styles.icon} alt="Иконка птицы" />
-      <p className={styles.description}>Press “space” for start</p>
+      {isBeginGame ? <StartGame /> : null}
     </main>
   )
 }
