ce6cadb1db874e6909714fa1a826123a1806f0e3
Alexander Shipulin <sania8600@gmail.com>
2023-10-21 15:44:23 +0700

    fix: заменена иконка птицы на svg -> png
    Задача: PIX-24

--- packages/client/src/assets/images/index.ts
+++ packages/client/src/assets/images/index.ts
@@ -4,7 +4,7 @@ import IconPipeLg from './pipe-lg.svg'
 import IconPipeXl from './pipe-xl.svg'
 import IconPipeReverse from './pipe-reverse.svg'
 import IconPipeReverseSm from './pipe-reverse-sm.svg'
-import IconBird from './yellowbird.svg'
+import IconBird from './yellowbird.png'
 import IconGameOver from './game-over.svg'
 
 export {
