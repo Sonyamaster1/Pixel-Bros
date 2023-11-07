22268c5ad2d9366006b223321fa4e3ce6de04a81
Митрофанова Софья <mitrofanovasofya@MacBook-Air-Mitrofanova.local>
2023-11-07 09:27:34 +0500

    PIX-35: lint err

--- packages/client/src/components/game_engaine/GameEngaine.test.tsx
+++ packages/client/src/components/game_engaine/GameEngaine.test.tsx
@@ -6,7 +6,9 @@ describe('тест для движка', () => {
   let component: renderer.ReactTestRenderer
 
   beforeEach(() => {
-    component = renderer.create(<GameEngaine />)
+    component = renderer.create(
+      <GameEngaine handleGameOver={() => console.log('test')} />
+    )
   })
   it('create snapshot', () => {
     expect(component.toJSON()).toMatchSnapshot()
