e3edfe27be9207f472badd1e7ea3cca84237b1ec
bokhodir <daikoziyev.baha@gmail.com>
2023-11-26 12:53:00 +0600

    заменил проверку на наличие dom, в пользу переменной окружения

--- packages/client/src/components/game_engaine/SpritesLoader.ts
+++ packages/client/src/components/game_engaine/SpritesLoader.ts
@@ -12,7 +12,7 @@ const Sprites = {
 
 function SpritesLoader(src: string): HTMLImageElement | undefined {
   // Проверка на наличие глобальных обьектов для серверного рендеринга
-  if (typeof window === 'undefined' || typeof document === 'undefined') return
+  if (import.meta.env.SSR) return
   const img = new Image()
   img.src = src
   return img
