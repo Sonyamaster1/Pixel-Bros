5ca8011731e9b09668a3947fd23445433423c01a
J4rCen <d_davletov@internet.ru>
2023-12-13 16:23:25 +0300

    fix

--- packages/server/router.ts
+++ packages/server/router.ts
@@ -1,9 +1,20 @@
 import { Router } from 'express'
+import cors from 'cors'
+import bodyParser from 'body-parser'
 import UserThemeRouter from './ThemeBD/ThemeRouters/UserThemeRouter'
 import SiteThemeRouter from './ThemeBD/ThemeRouters/SiteThemeRouter'
 import EmojiRouter from './EmojiDB/EmojiRouter/EmojiRouter'
 
 const router: Router = Router()
+
+router.use(cors())
+
+router.use(bodyParser.json({ type: 'application/json' }))
+router.use(
+  bodyParser.urlencoded({
+    extended: false,
+  })
+)
 
 router.use('/emoji', EmojiRouter)
 router.use('/user/theme', UserThemeRouter)
